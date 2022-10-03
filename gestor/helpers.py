99949dabd2fc0214b27651eaf283fe3d8a3caac9
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-03 12:36:17 +0200

    helpers terminado

--- gestor/helpers.py
+++ gestor/helpers.py
@@ -1,26 +1,27 @@
-import re
 import os
-import platform
-
+import re #para comrobar si lo que hemos hecho está bien
+import platform #detecta el sistema operativo que queremos y lo adapta para poder trabajar con él
 
 def limpiar_pantalla():
     os.system('cls') if platform.system() == "Windows" else os.system('clear')
 
-
-def leer_texto(longitud_min=0, longitud_max=100, mensaje=None):
-    print(mensaje) if mensaje else None
+#para poder leer un texto fácilmente
+def leer_texto(longitud_min = 0, longitud_max = 100, mnsj = None):
+    print(mnsj) if mnsj else None
     while True:
         texto = input("> ")
-        if len(texto) >= longitud_min and len(texto) <= longitud_max:
+        if longitud_min <= len(texto) <= longitud_max:
             return texto
 
-
+#comprobamos dni
 def dni_valido(dni, lista):
     if not re.match('[0-9]{2}[A-Z]$', dni):
-        print("DNI incorrecto, debe cumplir el formato.")
+        print("DNI incorrecto, no cumple el formato establecido")
         return False
     for cliente in lista:
         if cliente.dni == dni:
-            print("DNI utilizado por otro cliente.")
+            print("DNI ya en uso por otro cliente")
             return False
     return True
+
+    
